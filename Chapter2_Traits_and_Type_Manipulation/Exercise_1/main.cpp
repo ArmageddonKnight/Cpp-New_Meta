104f4f5098b6b4dee51aa67e57f3568ab8cfb0cc
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-05-07 01:52:00 -0400

    Finished Exercise 2.1.

--- Chapter2_Traits_and_Type_Manipulation/Exercise_1/main.cpp
+++ Chapter2_Traits_and_Type_Manipulation/Exercise_1/main.cpp
@@ -1,20 +1,37 @@
 #define BOOST_TEST_MODULE Exercise_2_1
 
 #include <boost/test/unit_test.hpp>
-
 #include <boost/static_assert.hpp>
 
-#include <boost/type_traits/is_compound.hpp>
-
-template < typename _ArgC, 
-           typename _ArgA, 
-           typename _ArgB >
-struct replace_type
-{
-        BOOST_STATIC_ASSERT(boost::is_compound < _ArgC > ::value);
-};
+#include "replace_type.hpp"
 
 BOOST_AUTO_TEST_CASE(exercise_2_1)
 {
-        
+        BOOST_STATIC_ASSERT(
+                boost::is_same < 
+                        replace_type_t < 
+                                void *, 
+                                void, 
+                                int 
+                        >,
+                        int *
+                > ::value);
+        BOOST_STATIC_ASSERT(
+                boost::is_same <
+                        replace_type_t < 
+                                int const * [10],
+                                int const,
+                                long
+                        >,
+                        long * [10]
+                > ::value);
+        BOOST_STATIC_ASSERT(
+                boost::is_same <
+                        replace_type_t <
+                                char & (*)(),
+                                char &,
+                                long &
+                        >,
+                        long & (*)()
+                > ::value);
 }
---
+++ Chapter2_Traits_and_Type_Manipulation/Exercise_1/replace_type.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/conditional.hpp>
+#include <boost/type_traits/add_pointer.hpp>
+#include <boost/type_traits/add_lvalue_reference.hpp>
+#include <boost/type_traits/add_rvalue_reference.hpp>
+
+template < typename _ArgC, typename _ArgA, typename _ArgB >
+struct replace_type
+{
+        typedef boost::conditional_t < 
+                boost::is_same < _ArgC, _ArgA > ::value,
+                _ArgB, _ArgC
+        > type;
+}; // Primary Base Template
+
+template < typename _ArgC, typename _ArgA, typename _ArgB >
+using replace_type_t = typename replace_type < _ArgC, _ArgA, _ArgB > ::type;
+
+template < typename _ArgC, typename _ArgA, typename _ArgB >
+struct replace_type < _ArgC * , _ArgA, _ArgB >
+{
+        typedef boost::conditional_t <
+                boost::is_same < _ArgC * , _ArgA > ::value,
+                _ArgB, 
+                boost::add_pointer_t < 
+                        replace_type_t <
+                                _ArgC, _ArgA, _ArgB
+                        >
+                >
+        > type;
+}; // Partial Specialization - Pointer
+
+template < typename _ArgC, typename _ArgA, typename _ArgB >
+struct replace_type < _ArgC & , _ArgA, _ArgB >
+{
+        typedef boost::conditional_t <
+                boost::is_same < _ArgC & , _ArgA > ::value,
+                _ArgB, 
+                boost::add_lvalue_reference_t < 
+                        replace_type_t <
+                                _ArgC, _ArgA, _ArgB
+                        >
+                >
+        > type;
+}; // Partial Specialization - LValue Reference
+
+template < typename _ArgC, typename _ArgA, typename _ArgB >
+struct replace_type < _ArgC &&, _ArgA, _ArgB >
+{
+        typedef boost::conditional_t <
+                boost::is_same < _ArgC &&, _ArgA > ::value,
+                _ArgB, 
+                boost::add_rvalue_reference_t < 
+                        replace_type_t <
+                                _ArgC, _ArgA, _ArgB
+                        >
+                >
+        > type;
+}; // Partial Specialization - RValue Reference
+
+template < typename _ArgC, typename _ArgA, typename _ArgB, size_t N >
+struct replace_type < _ArgC [N], _ArgA, _ArgB >
+{
+        typedef boost::conditional_t <
+                boost::is_same < _ArgC [N], _ArgA > ::value,
+                _ArgB,
+                replace_type_t < _ArgC, _ArgA, _ArgB > 
+        > type [N];
+};
+
+template < typename _ArgC, typename _ArgA, typename _ArgB, typename ..._VArg >
+struct replace_type < _ArgC (*)(_VArg...), _ArgA, _ArgB >
+{
+        typedef replace_type_t < _ArgC, _ArgA, _ArgB > (*type)(
+                replace_type_t < _VArg, _ArgA, _ArgB > ...); 
+};
