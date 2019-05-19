26e027c04a2c40c17e3abfa0ca8a29400283398f
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-05-18 23:08:11 -0400

    Made the build more generic.

--- addExerciseTest.cmake
+++ addExerciseTest.cmake
@@ -1,3 +1,10 @@
+function(add_all_exercises)
+        file(GLOB EXERCISES Exercise*)
+        foreach(exercise ${EXERCISES})
+                add_subdirectory(${exercise})
+        endforeach()
+endfunction()
+
 function(add_exercise_test)
         file(RELATIVE_PATH EXERCISE_REL_PATH 
                 ${CMAKE_SOURCE_DIR}
@@ -11,4 +18,5 @@ function(add_exercise_test)
         add_test(NAME ${EXERCISE_NAME} COMMAND ./${EXERCISE_NAME}
                 WORKING_DIRECTORY 
                 ${CMAKE_CURRENT_BINARY_DIR})
+        message("Added ${EXERCISE_NAME} to the test suite")
 endfunction()
